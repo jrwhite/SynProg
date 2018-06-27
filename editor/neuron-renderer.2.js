5bf7e43c217be6789497c34b1b1199237788fdfa
jrwhite <jrwhite20@gmail.com>
2018-06-26 19:59:48 -0500

    more more d3

--- editor/neuron-renderer.2.js
+++ editor/neuron-renderer.2.js
@@ -2,46 +2,28 @@ var d3 = require("d3")
 var d3Shape = require("d3-shape")
 
 var createSvg = function () {
-    var arcData = [
-        { "length": 50, "eccentricity": 2}
-    ]
+    var arcData = { "length": 50, "ecc": 2}
     
     var arcFunction = d3.arc()
         // .innerRadius(function(d) { return d.length })
         .innerRadius(0)
         // .outerRadius(function(d) { return d.length })
-        .outerRadius(50)
-        .startAngle(0)
-        .endAngle(Math.PI / 2)
+        .outerRadius((d,i) => (d.length))
+        .startAngle(Math.PI / 2)
+        .endAngle(3 * Math.PI / 2)
 
     var svgContainer = d3.select("body").append("svg")
         .attr("width", 200)
         .attr("height", 200)
     
-    var arcPath = svgContainer.append("path")
-        .datum({
-            "length": 50,
-            "ecc": 2
-        })
+    var arcPaths = svgContainer.selectAll("path")
+        .data([arcData])
+        .enter().append("path")
         .attr("d", arcFunction)
         .attr("stroke", "blue")
         .attr("stroke-width", 2)
         .attr("fill", "none")
 }
 
-function arcSetter(d) {
-    var rMaj = d.length,
-        rMin = 0 // debug
-    
-    console.log(rMaj)
-    
-    var arc = d3.arc()
-        .innerRadius(rMin)
-        .outerRadius(rMaj)
-        .startAngle(0)
-        .endAngle(Math.PI / 2)
-    
-    return arc
-}
 
 createSvg()
